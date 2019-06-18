0b28fcf196e6f5ffdd78564280b7192bd330125c
Adam Grodahl <adamgrodahl@Adams-MBP.hsd1.or.comcast.net>
2019-06-18 12:01:28 -0700

    spotify keys added

--- liri-node-app/liri/liri.js
+++ liri-node-app/liri/liri.js
@@ -1,6 +1,6 @@
 //Set evironmental varaibles into process.env from and .env file
 
-require("dontenv").config();
+var dotenv = require("dontenv").config();
 
 //Getting access to the npms we need each of the functions we will be doing. We will need axois, spotify, file sharing, moment, and the Spotify API Keys in my keys.js file. 
 
@@ -12,3 +12,4 @@ var spotify = new spotify(spotifyKeys.spotify);
 
 //access to the command line arguements
 const [node,file, ...args] = process.argv;
+
