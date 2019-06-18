f30c1b24d01d17d97daa0902c99136967fa954f1
Adam Grodahl <adamgrodahl@Adams-MBP.hsd1.or.comcast.net>
2019-06-18 11:33:28 -0700

    first commit

---
+++ assets/axios.js
@@ -0,0 +1,10 @@
+//grab Bands in Town axios package
+var axios = require("axios");
+
+//axios.get function
+axios
+    .get("http://www.artists.bandsintown.com/bandsintown-api")
+    .then(function(response){
+        //console.log to make sure the package was successful
+        console.log(response.data);
+    })
---
+++ assets/liri.js
@@ -0,0 +1,4 @@
+require("dontenv").config();
+
+var keys = require ("./keys.js");
+var spotify = new spotify(keys.spotify);
